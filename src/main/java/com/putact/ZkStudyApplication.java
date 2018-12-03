fc17eb421bf8e635f82326812ea26ba6a6c37882
housezhang <housezhang@tencent.com>
2018-12-03 19:37:26 +0800

    first commit

---
+++ src/main/java/com/putact/ZkStudyApplication.java
@@ -0,0 +1,12 @@
+package com.putact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZkStudyApplication.class, args);
+	}
+}
